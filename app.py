4c17bd4293bf4c9f3195661a7665e0a2f9c0a34c
Longe/Longmin <2220524364@qq.com>
2026-03-23 05:54:22 +0800

    Fix audio autoplay with context initialization

--- app.py
+++ app.py
@@ -650,7 +650,7 @@ st.markdown(f"""
     .stChatInput {{
         border-radius: 20px !important;
         border: 1px solid rgba(0,0,0,0.3) !important;
-        background-color: rgba(12,13,22,0.9) !important;
+        background-color: rgba(17,18,27,0.9) !important;
         font-family: 'Manrope', sans-serif !important;
         font-size: 16px !important;
         font-weight: 400 !important;
